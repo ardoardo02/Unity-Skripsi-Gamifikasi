da22fb5e314eba6e6f74d4d01d2b66cf457396cc
ardoardo02 <ardoardo02@yahoo.com>
2024-01-30 22:50:38 +0700

    making panels to be prefabs

--- QuranMemorizer/Assets/Scripts/Intro.cs
+++ QuranMemorizer/Assets/Scripts/Intro.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour
 {
+    [SerializeField] GameObject UsernamePanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("Welcome");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Welcome()
     {
-        
+        yield return new WaitForSeconds(3);
+        UsernamePanel.SetActive(true);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
