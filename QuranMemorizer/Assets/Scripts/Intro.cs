b9bf33da93551aef152693f3beada7ca28c03e95
ardoardo02 <ardoardo02@yahoo.com>
2024-02-21 04:58:49 +0700

    Finished Intro

--- QuranMemorizer/Assets/Scripts/Intro.cs
+++ QuranMemorizer/Assets/Scripts/Intro.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Intro : MonoBehaviour
 {
-    [SerializeField] GameObject UsernamePanel;
+    [Header("Character Panel")]
+    [SerializeField] GameObject CharacterPanel;
+    [SerializeField] Button BoyButton;
+    [SerializeField] Button GirlButton;
+
+    [Header("Username Panel")]
+    // [SerializeField] GameObject UsernamePanel;
+    [SerializeField] GameObject BoyImg;
+    [SerializeField] GameObject GirlImg;
+    [SerializeField] TMP_InputField UsernameInput;
+    [SerializeField] TMP_Text FirstParagraphStory;
+    [SerializeField] TMP_Text SecondParagraphStory;
+    [SerializeField] Button UsernameButton;
+
+    string CharacterSelected;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +31,62 @@ public class Intro : MonoBehaviour
     IEnumerator Welcome()
     {
         yield return new WaitForSeconds(3);
-        UsernamePanel.SetActive(true);
+        CharacterPanel.SetActive(true);
     }
 
-    public void LoadScene()
+    // update character
+    public void OnCharacterSelected(string character)
     {
-        SceneManager.LoadScene("MainMenu");
+        CharacterSelected = character;
+
+        // update boy button's normal color if boy is selected
+        if (character == "boy") {
+            ColorBlock cb = BoyButton.colors;
+            cb.normalColor = new Color32(74, 183, 184, 255);
+            BoyButton.colors = cb;
+
+            cb = GirlButton.colors;
+            cb.normalColor = new Color32(80, 101, 108, 255);
+            GirlButton.colors = cb;
+
+            BoyImg.SetActive(true);
+            GirlImg.SetActive(false);
+        } else {
+            ColorBlock cb = GirlButton.colors;
+            cb.normalColor = new Color32(74, 183, 184, 255);
+            GirlButton.colors = cb;
+
+            cb = BoyButton.colors;
+            cb.normalColor = new Color32(80, 101, 108, 255);
+            BoyButton.colors = cb;
+
+            GirlImg.SetActive(true);
+            BoyImg.SetActive(false);
+        }
+    }
+
+    // update value when input field is edited
+    public void OnUsernameValueEdit()
+    {
+        string username = UsernameInput.text;
+        
+        if (username.Length == 0) username = "(nama kamu)";
+
+        FirstParagraphStory.text = "Ada seorang anak <b>laki-laki</b> kecil yang tinggal di desa kecil di Indonesia, namanya <b><color=#00AAAB>" + username + "</color></b>. <b><color=#00AAAB>" + username + "</color></b> suka banget dengan lantunan <b>Al-Quran</b> dan mempunyai mimpi untuk tampil di kompetisi lantunan <b>Al-Quran</b> setiap tahunnya di desanya. Kompetisinya tuh keren banget dan banyak banget orang dari desa dan kota-kota tetangga yang datang nonton.";
+        SecondParagraphStory.text = "Tapi tentu saja, nggak gampang banget buat  <b><color=#00AAAB>" + username + "</color></b> buat siap-siap tampil di sana. Dia harus hafal <b>Al-Quran</b> sampai mampu mengesankan. Hanya dalam beberapa bulan,  <b><color=#00AAAB>" + username + "</color></b> memulai rutinitasnya untuk menghafal ayat-ayat <b>Al-Quran</b> setiap hari.";
+
+        if (UsernameInput.text.Length >= 4 && UsernameInput.text.Length <= 12){
+            UsernameButton.interactable = true;
+        } else {
+            UsernameButton.interactable = false;
+        }
+    }
+
+    public void OnSubmit()
+    {
+        PlayerPrefs.SetString("Character", CharacterSelected);
+        PlayerPrefs.SetString("Username", UsernameInput.text);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 }
