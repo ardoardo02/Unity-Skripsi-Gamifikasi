26ff8931f4a369aa9770580d22545e7d6a8b8b4d
ardoardo02 <ardoardo02@yahoo.com>
2024-03-01 03:58:16 +0700

    Question Type 1 Gameloop

--- QuranMemorizer/Assets/Scripts/GameManager.cs
+++ QuranMemorizer/Assets/Scripts/GameManager.cs
@@ -1,15 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     int currentSurah;
     int currentAyah;
 
+    int score;
+    int selectedQuestionType;
+
+    string multipleChoiceAnswer;
+    string clickedMultipleChoiceAnswer;
+
+    [SerializeField] QuestionDatabase questionDatabase;
+    [SerializeField] Button submitButton;
+    [SerializeField] TMP_Text scoreText;
+
+    [Header("Question Type 1")]
+    [SerializeField] GameObject questionType1;
+    [SerializeField] TMP_Text mainQuestionText;
+    [SerializeField] Text secondaryQuestionText;
+    [SerializeField] Text answerText1_1;
+    [SerializeField] Text answerText1_2;
+    [SerializeField] Text answerText1_3;
+    [SerializeField] Text answerText1_4;
+    List<Text> answerTexts1 = new List<Text>();
+
+    [Header("Question Type 2")]
+    [SerializeField] GameObject questionType2;
+    [SerializeField] TMP_Text mainQuestionText2;
+    [SerializeField] Text answerText2_1;
+    [SerializeField] Text answerText2_2;
+    [SerializeField] Text answerText2_3;
+    [SerializeField] Text answerText2_4;
+    List<Text> answerTexts2 = new List<Text>();
+
     private void Awake() {
         currentSurah = PlayerPrefs.GetInt("SurahNumber", 999);
         currentAyah = PlayerPrefs.GetInt("AyahNumber", 999);
+
+        score = 0;
+        scoreText.text = score.ToString();
+
+        answerTexts1.Add(answerText1_1);
+        answerTexts1.Add(answerText1_2);
+        answerTexts1.Add(answerText1_3);
+        answerTexts1.Add(answerText1_4);
+
+        answerTexts2.Add(answerText2_1);
+        answerTexts2.Add(answerText2_2);
+        answerTexts2.Add(answerText2_3);
+        answerTexts2.Add(answerText2_4);
+
+        // Add listener to the answer buttons
+        submitButton.onClick.AddListener(OnSubmitButtonClicked);
+
+        for (int i = 0; i < answerTexts1.Count; i++) {
+            int index = i;
+            answerTexts1[i].GetComponentInParent<Button>().onClick.AddListener(() => OnAnswerButtonClicked(index + 1));
+        }
     }
 
     // Start is called before the first frame update
@@ -21,11 +74,110 @@ public class GameManager : MonoBehaviour
         
         Debug.Log("Current Surah: " + currentSurah);
         Debug.Log("Current Ayah: " + currentAyah);
+
+        GetQuestionType1();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void GetQuestionType1() {
+        selectedQuestionType = 1;
+
+        int questionBasedOnAyah = currentAyah * 2;
+        if (questionBasedOnAyah > questionDatabase.questions.Count) {
+            questionBasedOnAyah = questionDatabase.questions.Count;
+        }
+        // Randomly select a question from the database
+        QuestionDatabase.Question currentQuestion = questionDatabase.questions[Random.Range(0, questionBasedOnAyah)];
+
+        // Display question text
+        mainQuestionText.text = currentQuestion.mainQuestionText;
+        secondaryQuestionText.GetComponent<ArabicFixer>().fixedText = currentQuestion.secondaryQuestionText;
         
+        // Get the correct answer
+        multipleChoiceAnswer = questionDatabase.options[currentQuestion.correctAnswerIndex];
+
+        // Randomize the options
+        List<string> randomizedOptions = new List<string>(questionDatabase.options);
+        randomizedOptions = randomizedOptions.OrderBy(x => UnityEngine.Random.value).ToList(); // Use OrderBy with a random value to shuffle the list
+        
+        // Display options
+        bool isAnswerSet = false;
+        for (int i = 0; i < answerTexts1.Count; i++) {
+            answerTexts1[i].GetComponent<ArabicFixer>().fixedText = randomizedOptions[i];
+            if (!isAnswerSet && randomizedOptions[i] == multipleChoiceAnswer) {
+                isAnswerSet = true;
+            }
+        };
+
+        // Randomly place the correct 
+        if (!isAnswerSet) {
+            int randomAnswerIndex = Random.Range(0, 4);
+            if (randomAnswerIndex == 0) {
+                answerText1_1.GetComponent<ArabicFixer>().fixedText = multipleChoiceAnswer;
+            } else if (randomAnswerIndex == 1) {
+                answerText1_2.GetComponent<ArabicFixer>().fixedText = multipleChoiceAnswer;
+            } else if (randomAnswerIndex == 2) {
+                answerText1_3.GetComponent<ArabicFixer>().fixedText = multipleChoiceAnswer;
+            } else if (randomAnswerIndex == 3) {
+                answerText1_4.GetComponent<ArabicFixer>().fixedText = multipleChoiceAnswer;
+            }
+        }
+
+        questionType1.SetActive(true);
+        Debug.Log("Correct answer: " + multipleChoiceAnswer);
+    }
+
+    public void OnAnswerButtonClicked(int answerIndex) {
+        // Store the clicked answer
+        clickedMultipleChoiceAnswer = answerTexts1[answerIndex - 1].text;
+
+        // Reset the color of all answers
+        for (int i = 0; i < answerTexts1.Count; i++) {
+            answerTexts1[i].GetComponentInParent<Image>().color = new Color32(255, 255, 255, 255);
+            answerTexts1[i].color = new Color32(51, 51, 51, 255);
+        }
+
+        // Change the color of the clicked answer
+        answerTexts1[answerIndex - 1].GetComponentInParent<Image>().color = new Color32(0, 169, 171, 255);
+        answerTexts1[answerIndex - 1].color = new Color32(255, 255, 255, 255);
+
+        submitButton.interactable = true;
+    }
+
+    public void OnSubmitButtonClicked() {
+        StartCoroutine(OnSubmitButtonClicked_Coroutine());
+    }
+
+    IEnumerator OnSubmitButtonClicked_Coroutine() {
+        submitButton.interactable = false;
+        ColorBlock colors = submitButton.colors;
+
+        // Check if the clicked answer is the correct answer
+        if (clickedMultipleChoiceAnswer == multipleChoiceAnswer) {
+            colors.disabledColor = new Color32(100, 193, 126, 255); // Correct answer color
+            score += 100;
+            scoreText.text = score.ToString();
+        } else {
+            colors.disabledColor = new Color32(171, 0, 0, 255); // Wrong answer color
+        }
+
+        submitButton.colors = colors;
+
+        // Wait for 2 seconds before changing the color back to normal
+        yield return new WaitForSeconds(2);
+        colors.disabledColor = new Color32(185, 185, 185, 255);
+        submitButton.colors = colors;
+
+        // Reset the color of all answers
+        for (int i = 0; i < answerTexts1.Count; i++) {
+            answerTexts1[i].GetComponentInParent<Image>().color = new Color32(255, 255, 255, 255);
+            answerTexts1[i].color = new Color32(51, 51, 51, 255);
+        }
+
+        clickedMultipleChoiceAnswer = "";
+        multipleChoiceAnswer = "";
+        selectedQuestionType = 0;
+        questionType1.SetActive(false);
+
+        GetQuestionType1();
     }
 }
---
+++ QuranMemorizer/Assets/Scripts/QuestionDatabase.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionDatabase : MonoBehaviour
+{
+    // Define a custom data structure to represent a question
+    public class Question
+    {
+        public string mainQuestionText;
+        public string secondaryQuestionText;
+        public int correctAnswerIndex;
+
+        public Question(string mainQuestionText, string secondaryQuestionText, int correctAnswerIndex)
+        {
+            this.mainQuestionText = mainQuestionText;
+            this.secondaryQuestionText = secondaryQuestionText;
+            this.correctAnswerIndex = correctAnswerIndex;
+        }
+    }
+
+    // List to store your questions
+    public List<Question> questions = new List<Question>();
+
+    // Shared array of options
+    public string[] options;
+
+    void Awake()
+    {
+        // Populate the question database with sample questions and options
+        questions.Add(new Question("What is the capital of", "France", 0));
+        questions.Add(new Question("Which country is known as the", "Land of the Rising Sun", 4));
+        questions.Add(new Question("What is the largest planet in our", "solar system", 8));
+        questions.Add(new Question("Which planet is known as the", "Red Planet", 10));
+        questions.Add(new Question("Who painted the", "Mona Lisa", 16));
+        questions.Add(new Question("Who wrote the famous play", "Romeo and Juliet", 20));
+        questions.Add(new Question("What is the chemical symbol for", "Water", 24));
+        questions.Add(new Question("What is the largest mammal on", "Earth", 28));
+        questions.Add(new Question("Which of the following is NOT", "a primary color", 32));
+        questions.Add(new Question("What is the currency of", "Japan", 36));
+
+        // Set shared options
+        options = new string[] { 
+            "Paris", "London", "Rome", "Berlin", // Question 1, answer index 0
+            "Japan", "India", "China", "South Korea", // Question 2, answer index 4
+            "Jupiter", "Saturn", "Mars", "Earth", // Question 3, answer index 8
+            "Moon", "Venus", "Mercury", "Sun", // Question 4, answer index 10
+            "Leonardo da Vinci", "Frida Kahlo", "Vincent van Gogh", "Pablo Picasso", // Question 5, answer index 16
+            "William Shakespeare", "Oscar Wilde", "George Bernard Shaw", "Samuel Beckett", // Question 6, answer index 20
+            "H2O", "CO2", "NaCl", "O2", // Question 7, answer index 24
+            "Blue Whale", "Elephant", "Giraffe", "Hippopotamus", // Question 8, answer index 28
+            "Yellow", "Red", "Blue", "Green", // Question 9, answer index 32
+            "Yen", "Dollar", "Euro", "Pound", // Question 10, answer index 36
+        };
+    }
+
+    // Example usage
+    /*
+    void Start()
+    {
+        // Access the first question
+        Question currentQuestion = questions[0];
+
+        // Display question text
+        Debug.Log("Question: " + currentQuestion.mainQuestionText);
+
+        // Display options
+        foreach (string option in options)
+        {
+            Debug.Log("Option: " + option);
+        }
+
+        // Get the correct answer
+        int correctAnswerIndex = currentQuestion.correctAnswerIndex;
+        Debug.Log("Correct Answer Index: " + correctAnswerIndex);
+        Debug.Log("Correct Answer: " + options[correctAnswerIndex]);
+    }
+    */
+}
+
